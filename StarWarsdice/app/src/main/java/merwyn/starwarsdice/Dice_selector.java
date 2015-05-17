bd549871aa3be25ce1c558ead74d25b626647760
Pierre F <pierre.fernbach@gmail.com>
2015-05-17 22:01:20 +0200

    Add first star wars dice try

---
+++ StarWarsdice/app/src/main/java/merwyn/starwarsdice/Dice_selector.java
@@ -0,0 +1,18 @@
+package merwyn.starwarsdice;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class Dice_selector extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dice_selector);
+    }
+
+
+}
